7f7f70fe83d5a7418ad08456916e87fa6b3700b8
imehmetgogus <mehmet.gogus@egeacademy.ca>
2025-03-04 19:05:53 +0000

    xx

---
+++ web-embed/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.getElementById('notification').style.left = '0';
+    }, 1500);
+    
+    setTimeout(function() {
+        document.getElementById('notification').style.left = '-300px';
+    }, 6500);
+});
